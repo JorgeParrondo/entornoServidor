b5fdd06f4b589c8ed8550726896c232614b31acc
JorgeParrondo <jorgeparron2@gmail.com>
2025-11-21 12:13:03 +0100

    examen modelo 2024 empezado

---
+++ EJERCICIOS_EXAMEN/examen1ev_v0/funciones.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Checks if the provided username and password are valid.
+ *
+ * @param string $username The username to validate.
+ * @param string $password The password to validate.
+ * @return bool Returns true if the username and password are valid, false otherwise.
+ */
+function accesoValido($username, $password): bool
+{
+    // COMPLETAR  +++++++++++++++++++++++++++++++
+    return true;
+}
+
+/**
+ * Records a new access for the given username.
+ *
+ * @param string $username The username for which to record the access.
+ * @return int The result of the access recording operation.
+ */
+function anotarNuevoAcceso($username):int{
+
+     // COMPLETAR  +++++++++++++++++++++++++++++++
+    return 0;
+}
+
+/**
+ * Registers a user with a given username and time.
+ *
+ * @param string $username The username of the user to register.
+ * @param int $time The time associated with the registration.
+ */
+function registra($username,$time){
+     // COMPLETAR  +++++++++++++++++++++++++++++++
+    return;
+}
---
+++ EJERCICIOS_EXAMEN/examen1ev_v0/index.php
@@ -0,0 +1,10 @@
+<?php
+include_once 'funciones.php';
+session_start();
+
+echo '<H1> NO HAGO NADA &#128542; </H1>';
+
+// COMPLETAR  +++++++++++++++++++++++++++++++
+if ( isset($_SESSION['tiempolimite']) ){
+ 
+}
