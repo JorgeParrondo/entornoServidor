d350c5ab05e78091ccd6d5c90580f6d3653c2ebe
JorgeParrondo <jorgeparron2@gmail.com>
2025-12-03 17:16:47 +0100

    examen repetido

---
+++ planteamiento/eje01/funciones.php
@@ -0,0 +1,62 @@
+<?php
+
+include "dat/Cliente.php";
+
+/**
+ *  Lee el fichero de clientes y lo carga en un Array de objetos clientes
+ *  @return array - tabla asociativa con clave dni.
+ */
+
+function cargarTablaClientes (): array {
+
+    $tclientes = [];
+     // COMPLETAR
+
+    return $tclientes;
+
+}
+
+/**
+ * Escribe la tabla de objectos clientes en el fichero csv
+ * @param  $tabla - array de objectos
+ */
+
+function salvarTablaClientes(array $tabla){
+
+    $fich = fopen('dat/clientes.csv','w');
+    // COMPLETAR
+
+    fclose($fich);
+
+}
+
+/**
+ * Valida usuario y contraseña contra clientes.csv
+ * @param string $dni DNI del cliente
+ * @param string $clave Contraseña en texto plano
+ * @return true Si el usuario y la contraseña son correctas
+ */
+function validarCliente($dni, $clave) :bool{
+    
+    $tablacli = cargarTablaClientes();
+    // COMPLETAR
+
+    return false;
+}
+
+/**
+ * Anota los puntos logrados en la última partida 
+ * @param string $dni DNI del cliente a modificar
+ * @param int $puntos Puntos a almacenar
+ * @return true si han anotado los datos
+*/
+function anotarPuntos($dni,$puntos): bool {
+    $tablaCli = cargarTablaClientes();
+    // COMPLETAR
+
+    return false;
+}
+
+
+
+
